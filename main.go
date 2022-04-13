86cbbb2175940a170a9603e54284e5117811c192
GOCrazy7575 <83633399+gocrazygh@users.noreply.github.com>
2022-04-13 17:01:45 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
-package pnde
+package main
 
-func ifPalindrome(s string) (b bool) {
-	a := ""
+import "fmt"
 
-    Reverse := func(s string) (result string) {
+func Reverse(s string) (result string) {
 	for _,v := range s {
 	  result = string(v) + result
 	}
 	return 
     }
 
-	a = Reverse(s)
+func ifPalindrome(s string) (b bool) {
+	a := Reverse(s)
 	if s == a{
 		return true
 	}else {
 		return false
 	}
+}
+
+func main(){
+	a := ifPalindrome("racecar")
+	fmt.Println(a)
 }
