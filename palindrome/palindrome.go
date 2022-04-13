98efa1390e9869c2dcd0ba4a401c92a50e529e30
GOCrazy7575 <83633399+gocrazygh@users.noreply.github.com>
2022-04-13 22:57:26 +0800

    Update palindrome.go

--- palindrome/palindrome.go
+++ palindrome/palindrome.go
@@ -2,6 +2,7 @@ package palindrome
 
 import "fmt"
 
+//Reverses every character in the string
 func Reverse(s string) (result string) {
 	for _,v := range s {
 	  result = string(v) + result
@@ -9,6 +10,7 @@ func Reverse(s string) (result string) {
 	return 
     }
 
+//checkPwnd checks if the string is same as it is reversed
 func checkPwnd(s string) (b bool) {
 	a := Reverse(s)
 	if s == a{
