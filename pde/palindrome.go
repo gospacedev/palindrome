3a79d2e36d55d38de08ade1064b68cc4c39d56b7
GOCrazy7575 <83633399+gocrazygh@users.noreply.github.com>
2022-04-13 23:23:05 +0800

    Update and rename palindrome.go to pde/palindrome.go

---
+++ pde/palindrome.go
@@ -0,0 +1,20 @@
+package pde
+
+//Reverses every character in the string
+func Reverse(s string) (result string){
+	for _,v := range s {
+	  result = string(v) + result
+	}
+	return 
+    }
+
+//Check checks if the string is same as it is reversed
+func Check(s string) (b bool){
+	a := Reverse(s)
+	if s == a{
+		b = true
+	}else {
+		b = false
+	}
+	return
+}
