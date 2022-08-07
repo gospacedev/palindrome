c1dfa591e6b4369a2360cfbd10e1915dd62b17ca
GOCrazy7575 <83633399+gocrazygh@users.noreply.github.com>
2022-08-07 19:32:38 +0800

    Update pal.go

--- pal.go
+++ pal.go
@@ -2,14 +2,14 @@ package pal
 
 //Reverses every character in the string
 func Reverse(s string) (result string){
-	for _,v := range s {
+	for _,v := range s {//Adding the string backwards
 	  result = string(v) + result
 	}
 	return 
     }
 
 //Check if the string is same as it is reversed
-func Check(s string) (b bool){
+func Check(s string) (b bool){// Checks the string when it's the same when reversed
 	a := Reverse(s)
 	if s == a{
 		b = true
